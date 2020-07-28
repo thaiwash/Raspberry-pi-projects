efd4c5f7b3d2bc89b86aca61a4dfb0f749442f0f
Taivas Gogoljuk <taivas.gogoljuk@gmail.com>
2020-07-28 13:58:11 +0300

    no comment

--- nodempucast/server.js
+++ nodempucast/server.js
@@ -1,3 +1,6 @@
+// Reseach how to convert gyro to threejs
+// gyro values are from -255 to 255
+
 var i2c = require('i2c-bus');
 var MPU6050 = require('i2c-mpu6050');
 var address = 0x68;
@@ -5,3 +8,23 @@ var i2c1 = i2c.openSync(1);
 var sensor = new MPU6050(i2c1, address);
 var data = sensor.readSync();
 console.log(data);
+
+
+const express = require('express')
+const app = express()
+const port = 3000
+const fs = require("fs")
+
+app.use(express.static('public'))
+
+app.get('/', function(req, res) {
+    res.send(fs.readFileSync("public/index.html"))
+})
+
+app.get('/sensor', function(req, res) {
+    res.send(sensor.readSync())
+})
+
+app.listen(port, function() {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
