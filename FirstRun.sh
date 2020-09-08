e78dd22e85c40f4b23a84ae624f3b03a9a1f287b
Taivas Gogoljuk <taivas.gogoljuk@gmail.com>
2020-09-08 16:31:32 +0100

    autocommit

---
+++ FirstRun.sh
@@ -0,0 +1,10 @@
+# install micro
+curl https://getmic.ro | bash
+sudo mv micro /usr/bin
+
+# install nodejs
+sudo npm install -y nodejs
+sudo npm install -y npm
+
+# install pulseaudio
+udo apt-get install pulseaudio
